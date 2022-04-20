acbafd641fec6167ce1f74a46fbbbb92cee52b38
thiswind <thiswind@gmail.com>
2022-04-20 17:20:43 +0000

    修改：format

--- Max.java
+++ Max.java
@@ -1,10 +1,11 @@
-public class Max{
-    public static int[] list={5,1,2,3,7,14,6};
-    public static void main(String[] args){
-        int max=-1;
-        for(int i=0;i<list.length;i++){
-            if(list[i]>max){
-                max=list[i];
+public class Max {
+    public static int[] list = { 5, 1, 2, 3, 7, 14, 6 };
+
+    public static void main(String[] args) {
+        int max = -1;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] > max) {
+                max = list[i];
             }
         }
         System.out.println(max);
